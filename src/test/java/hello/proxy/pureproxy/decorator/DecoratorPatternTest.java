9704f3f74f7c1b50d13980a2933ccca236a9dfa2
raccoonjeong <rana3782@gmail.com>
2023-01-11 20:16:01 +0900

    프록시 패턴과 데코레이터 패턴

---
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,38 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        RealComponent realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+
+    }
+
+    @Test
+    void decorator1() {
+        Component component = new RealComponent();
+        Component decorator = new MessageDecorator(component);
+        DecoratorPatternClient client = new DecoratorPatternClient(decorator);
+        client.execute();
+    }
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
