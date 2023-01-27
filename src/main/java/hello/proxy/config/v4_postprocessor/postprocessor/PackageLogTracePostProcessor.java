53f1683ee23b39a45da8a24eb0320c7ca593e35d
raccoonjeong <rana3782@gmail.com>
2023-01-27 14:13:47 +0900

    Section07 빈 후처리기 - 정리

---
+++ src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
@@ -0,0 +1,41 @@
+package hello.proxy.config.v4_postprocessor.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@Slf4j
+public class PackageLogTracePostProcessor implements BeanPostProcessor {
+
+    // 특정 패키지 하위에 있는 것에만 적용할 것임
+    private final String basePackage;
+    // 이 안에 포인트컷와 어드바이스가 들어간다.
+    private final Advisor advisor;
+
+    public PackageLogTracePostProcessor(String basePackage, Advisor advisor) {
+        this.basePackage = basePackage;
+        this.advisor = advisor;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("param beanName={} bean={}", beanName, bean.getClass());
+
+        // 프록시 적용 대상 여부 체크
+        // 프록시 적용 대상이 아니면 원본을 그대로 진행
+        String packageName = bean.getClass().getPackageName();
+        if(!packageName.startsWith(basePackage)) {
+            return bean;
+        }
+
+        // 프록시 대상이면 프록시를 만들어서 반환
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        proxyFactory.addAdvisor(advisor);
+
+        Object proxy = proxyFactory.getProxy();
+        log.info("create proxy: target={}, proxy={}", bean.getClass(), proxy.getClass());
+        return proxy;
+    }
+}
